646e6937dccc58105737a4ac593c677ff2bfc2d6
izypsy <reddy.nie11@gmail.com>
2026-05-15 14:26:11 +0530

    foundation

--- 1_foundations_izypsy/app.py
+++ 1_foundations_izypsy/app.py
@@ -18,11 +18,105 @@ def push(text):
         }
     )
 
-openai = OpenAI(api_key=os.getenv("OPENROUTER_API_KEY"), base_url="https://openrouter.ai/api/v1")
+def record_user_details(email):
+    push(f"Recording user details for {email}")
+    return {"recorded": "ok"}
 
-messages = [{"role": "user", "content": "What is 2+2?"}]
-response = openai.chat.completions.create(
-    model=os.getenv("GPT"),
-    messages=messages,
-)
-print(response.choices[0].message.content)
+def record_unknown_question(question):
+    push(f"I got a unknow question: {question}")
+    return {"recorded": "ok"}
+
+record_user_details_json = {
+    "name": "record_user_details",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "email": {
+                "type": "string",
+                "description": "The email address of this user"
+            }
+        },
+        "required": ["email"],
+        "additionalProperties": False
+    }
+}
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {
+                "type": "string",
+                "description": "The question that couldn't be answered"
+            },
+        },
+        "required": ["question"],
+        "additionalProperties": False
+    }
+}
+
+tools = [{"type": "function", "function": record_user_details_json},
+        {"type": "function", "function": record_unknown_question_json}]
+    
+
+class Izypsy:
+    def __init__(self):
+        self.openai = OpenAI(api_key=os.getenv("OPENROUTER_API_KEY"), base_url=os.getenv("OPENROUTER_BASE_URL"))
+        self.name = "Izypsy"
+        reader = PdfReader("../1_foundations/me/linkedin.pdf")
+        self.linkedin = ""
+        for page in reader.pages:
+            text = page.extract_text()
+            if text:
+                self.linkedin += text
+        with open("../1_foundations/me/summary.txt", "r", encoding="utf-8") as f:
+            self.summary = f.read()
+
+    def handle_tool_calls(self, tool_calls):
+        results = []
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            arguments = json.loads(tool_call.function.arguments)
+            print(f"Tool called: {tool_name}", flush=True)
+            tool = globals().get(tool_name)
+            result = tool(**arguments) if tool else {}
+            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+        return results
+
+    def system_prompt(self):
+        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
+particularly questions related to {self.name}'s career, background, skills and experience. \
+Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
+You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
+Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+
+        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
+        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
+        return system_prompt
+
+    def chat(self, message, history):
+        messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
+        done = False
+        while not done:
+            response = self.openai.chat.completions.create(model=os.getenv("GPT"), messages=messages, tools=tools)
+            print("########## response ##########", flush=True)
+            print(response.choices[0].message.content, flush=True)
+            if response.choices[0].finish_reason=="tool_calls":
+                message = response.choices[0].message
+                print(f"message: {message}", flush=True)
+                tool_calls = message.tool_calls
+                results = self.handle_tool_calls(tool_calls)
+                messages.append(message)
+                messages.extend(results)
+            else:
+                done = True
+        return response.choices[0].message.content
+
+if __name__ == "__main__":
+    izypsy = Izypsy()
+    gr.ChatInterface(izypsy.chat, type="messages").launch()
